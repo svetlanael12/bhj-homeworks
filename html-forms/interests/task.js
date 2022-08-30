eb06ee2ae7e57496e755e622e621400f7f874601
Svetlana <svetlana_el12@mail.ru>
2022-08-31 01:00:32 +0500

    interests(edit)

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,16 +1,27 @@
 (() => {
-    let checkbox = document.querySelectorAll('.interest');
+    let checkbox = document.querySelectorAll('input');
 
     checkbox.forEach((arr) => {
-        arr.addEventListener('click', function(e) {
-            if (arr.querySelector('.interest .interest__check').checked) {
-                arr.querySelectorAll('.interests_active .interest__check').forEach((item) => {
-                    item.checked = true;
-                })
+        arr.addEventListener('click', function() {
+            if (arr.closest('.interests_active') === null) {
+                let parent = arr.closest('.interest').querySelector('.interests_active');
+                if (arr.checked) {
+                    parent.querySelectorAll('.interest__check').forEach((elem) => {
+                        elem.checked = true;
+                    })
+                } else {
+                    parent.querySelectorAll('.interest__check').forEach((elem) => {
+                        elem.checked = false;
+                    })
+                }
             } else {
-                arr.querySelectorAll('.interests_active .interest__check').forEach((item) => {
-                    item.checked = false;
-                })
+                let array = Array.from(arr.closest('.interests_active').querySelectorAll('.interest__check'));
+
+                if (array.every((elem) => elem.checked)) {
+                    arr.closest('.interests_active').closest('.interest').querySelector('input').checked = true;
+                } else {
+                    arr.closest('.interests_active').closest('.interest').querySelector('input').checked = false;
+                }
             }
         })   
     }) 
